b2bdf2a27f26e6c52b4ae64e7b8150b838e9ad2c
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-12-21 18:42:20 -0600

    added edabit challenge solutions

--- js/javascript-practice.js
+++ js/javascript-practice.js
@@ -13,3 +13,30 @@ const helloWorld = greeting => greeting;
 
 
 const multiply = (x,y) => (x * y);
+
+
+//Edabit exercises
+
+function returnNegative(n){
+    if (n < 0) {
+        return n
+    } else {
+        return n * -1;
+    }
+}
+
+function isSameNum(num1, num2) {
+    return num1 === num2;
+}
+
+function findPerimeter(length, width) {
+    return (length * 2) + (width * 2);
+}
+
+function addition(num) {
+    return ++num;
+}
+
+function lessThan100(a, b) {
+    return (a + b) < 100;
+}
