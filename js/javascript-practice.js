71461fa65669efc01446bf9e581f6cddb274bdb4
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2022-02-10 15:14:53 -0600

    reverse number array exercise

--- js/javascript-practice.js
+++ js/javascript-practice.js
@@ -59,3 +59,9 @@ function highAndLow(numbers){
 }
 
 // console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
+
+function digitize(n) {
+    return Array.from (String(n), Number).reverse()
+}
+
+// console.log(digitize(12345));
