499e8123ca55121a58f7ee8171d4f6d74f3d21fc
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2022-02-16 11:48:08 -0600

    sum array of mixed type values exercise

--- js/javascript-practice.js
+++ js/javascript-practice.js
@@ -187,3 +187,18 @@ function rowSumOddNumbers(n) {
 
 // console.log(rowSumOddNumbers(42));
 // console.log(rowSumOddNumbers(10));
+
+
+function sumMix(x){
+    var sum = 0
+    for (var i = 0; i < x.length; i++) {
+        sum += parseFloat(x[i])
+    }
+    return sum
+}
+
+// console.log(sumMix([9, 3, '7', '3']));
+// console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]));
+// console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']));
+
+
