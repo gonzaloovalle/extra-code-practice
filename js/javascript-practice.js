3c2775e54d6c09223dc90654241fabdc0b0023f5
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2022-02-16 09:56:36 -0600

    sum of pyramid exercise

--- js/javascript-practice.js
+++ js/javascript-practice.js
@@ -177,4 +177,13 @@ var uniqueInOrder=function(iterable){
 // console.log(uniqueInOrder('ABBCcAD'));
 // console.log(uniqueInOrder([1, 2, 2, 3, 3]));
 
+function rowSumOddNumbers(n) {
+    var sum = 0
+    for (var i = 1; i <= n; i++) {
+        sum += i + 2
+    }
+    return sum
+}
 
+// console.log(rowSumOddNumbers(42));
+// console.log(rowSumOddNumbers(10));
