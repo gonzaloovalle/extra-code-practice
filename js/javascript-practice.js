bba5cd6224f61950a9229c5d409abe54b34f2c79
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2022-02-17 16:08:53 -0600

    find unique number exercise

--- js/javascript-practice.js
+++ js/javascript-practice.js
@@ -330,4 +330,12 @@ function domainName(url){
 // console.log(domainName("https://www.cnet.com"));
 
 
+function findUniq(arr) {
+    let repeated = arr.filter((item, index) => arr.indexOf(item) !== index)
+    return arr.filter((item)=> item !== repeated[0])[0]
+}
+
+// console.log(findUniq([1, 1, 1, 2, 1, 1]));
+// console.log(findUniq([0, 0, 0.55, 0, 0]));
+
 
