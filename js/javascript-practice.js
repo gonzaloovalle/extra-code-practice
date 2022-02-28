64237b5fe24da9e66674e88946d285435c40d8d3
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2022-02-28 12:32:37 -0600

    fibonacci sequence generator

--- js/javascript-practice.js
+++ js/javascript-practice.js
@@ -362,3 +362,19 @@ function basicOp(operation, value1, value2)
 // console.log(basicOp('-', 15, 18));
 // console.log(basicOp('*', 5, 5));
 // console.log(basicOp('/', 49, 7));
+
+
+function fibonacci(number) {
+    let n1 = 0, n2 = 1, nextTerm;
+    console.log("Fibonacci sequence: ");
+    for (let i = 0; i < number; i++) {
+        console.log(n1);
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm
+    }
+    return n2
+}
+
+console.log(fibonacci(4));
+console.log(fibonacci(10));
